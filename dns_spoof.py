213d76cd17fd711713a36be882b33cea1b666e56
Srijita Mondal <quokkaiscute1409@gmail.com>
2026-02-26 16:34:20 +0530

    Update dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,1 +1,8 @@
+from scapy.all import sniff, DNS, DNSQR
 
+def dns_monitor(packet):
+    if packet.haslayer(DNS) and packet.haslayer(DNSQR):
+        query = packet[DNSQR].qname.decode()
+        print(f"DNS Query: {query}")
+
+sniff(filter="udp port 53", prn=dns_monitor, store=False)
