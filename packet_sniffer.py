190fcd544dcd7fa866aec6d1b883de2bd317a9b9
Srijita Mondal <quokkaiscute1409@gmail.com>
2026-02-26 16:34:03 +0530

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,1 +1,10 @@
+from scapy.all import sniff, IP, TCP, UDP
 
+def process(packet):
+    if packet.haslayer(IP):
+        src = packet[IP].src
+        dst = packet[IP].dst
+        proto = "TCP" if packet.haslayer(TCP) else "UDP" if packet.haslayer(UDP) else "IP"
+        print(f"{src} -> {dst} | {proto}")
+
+sniff(filter="ip", prn=process, store=False)
