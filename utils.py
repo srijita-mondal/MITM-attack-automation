61e7cc241fefe9002be972559e02f14ac80f9060
Srijita Mondal <quokkaiscute1409@gmail.com>
2026-02-26 16:33:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+from scapy.all import ARP, Ether, srp
 
+def get_mac(ip):
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = broadcast / arp_request
+    answered = srp(packet, timeout=2, verbose=False)[0]
+    if answered:
+        return answered[0][1].hwsrc
+    return None
